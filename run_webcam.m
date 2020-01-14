779c5a76aa4a2af3050f979a07f3df3a4f3ba999
Martin Contreras <mcontr15@ucsc.edu>
2020-01-13 23:22:58 -0800

    made a googlenet detector

--- run_webcam.m
+++ run_webcam.m
@@ -2,10 +2,12 @@ clear all;
 clc;
 %%
 cam = webcam;
+cam.AvailableResolutions
 %cam.Resolution = '640x480';
 inputSize = [224 224 3];
 YOLO_Object_Classifier = load('YOLOv2_MBSize_10_MaxEpoch_20_InitLearnRate_1e-3_NumAnchors_15.mat');
 %%
+figure;
 for  i = 1:10000
     img = snapshot(cam);
     img = imresize(img,inputSize(1:2));
@@ -13,6 +15,8 @@ for  i = 1:10000
     if(~isempty(bboxes))
         img = insertObjectAnnotation(img,'rectangle',bboxes,scores); 
     end
+%     figure;
+%     img = imresize(img,[720 1080]); Martin's camera
     imshow(img);
 end 
 %%
