54eb55d958ee4b5373fcae1f82aaa040530d347e
Viet William Nguyen <vwnguyen@crm.ucsc.edu>
2020-01-14 19:42:10 -0800

    modular yolo file added functionality, renamed the object detectors to have a bit more information

--- run_webcam.m
+++ run_webcam.m
@@ -1,10 +1,10 @@
 clear all;
 clc;
 %%
-cam = webcam;
-%cam.Resolution = '640x480';
+cam = webcam
+cam.Resolution = '640x480';
 inputSize = [224 224 3];
-YOLO_Object_Classifier = load('YOLOv2_MBSize_10_MaxEpoch_20_InitLearnRate_1e-3_NumAnchors_15.mat');
+YOLO_Object_Classifier = load('YOLOv2_MBSize_10_MaxEpoch_20_InitLearnRate_1e-3_NumAnchors_15_Acc_11.mat');
 %%
 for  i = 1:10000
     img = snapshot(cam);
