1e997e5b2f65ae07707bf140198c393bdd768a59
Viet <vwnguyen@ucsc.edu>
2020-01-29 20:13:27 -0800

    run webcam runs fast as possible and maps coordinates

--- run_webcam.m
+++ run_webcam.m
@@ -2,74 +2,73 @@ clear all
 clc
 %%
 % clear cam;
-cam = webcam;
-cam.Resolution = '640x480';
-inputSize = [224 224 3];
+cam = webcam(2);
+cam.Resolution = '320x240';
+inputSize = [244 244 3];
 YOLO_Object_Classifier = load('resnet50_6.mat');
 bboxAreaThreshold = 1000;
+theta = 0;
+phi = 0;
+psi = 0;
+P_A_BORG = [ 0; 0; 0 ];
+origLength = 0;
 %%
 for  i = 1:10000
     img = snapshot(cam);
-    size(img);
-    
     img = imresize(img,inputSize(1:2));
     [bboxes,scores] = detect(YOLO_Object_Classifier.detector,img);
-    
     % filter by selecting the strongest bounding box in the case of
     % overlapping ones
-    
     [bboxes,scores] = selectStrongestBbox(bboxes,scores,'OverlapThreshold',0.2);
-    bboxAreas = bboxes(:,3) .* bboxes(:,4);
-    invalidAreaArray = bboxAreas < bboxAreaThreshold;  
-    indicesToRemove = find(invalidAreaArray);
     
-    bboxes(indicesToRemove,:) = [];
-    scores(indicesToRemove,:) = [];
+    % area filtering
+%     bboxAreas = bboxes(:,3) .* bboxes(:,4);
+%     invalidAreaArray = bboxAreas < bboxAreaThreshold;  
+%     indicesToRemove = find(invalidAreaArray);
+%     bboxes(indicesToRemove,:) = [];
+%     scores(indicesToRemove,:) = [];
     
-    xLeftTop = bboxes(:,1)   + (bboxes(:,3)/2) ;
-    yLeftBottom = bboxes(:,2)  + (bboxes(:,4)/2);
     
-    % plot the initial annotations
+    xCenter = bboxes(:,1)   + (bboxes(:,3)/2) ; % note bboxes(:,1) = LHS X
+    yCenter = bboxes(:,2)  + (bboxes(:,4)/2); % note bboxes(:,2) = Y from bottom
+    
+    % plot the initial confidence annotations
     if (~isempty(bboxes) && (length(bboxes)>0))
         img = insertObjectAnnotation(img,'rectangle',bboxes,scores); 
+        imshow(img);
+        if length(xCenter) > 0 
+            hold on;
+            plot(xCenter , yCenter  , 'r*', 'LineWidth', 2, 'MarkerSize', 3);
+            for j = 1:length(xCenter)
+                P_A(j,:) = mapToRobotBase(theta,phi,psi,xCenter(j,1),yCenter(j,1),P_A_BORG);
+                coordinates(j) = cellstr(strcat(int2str(P_A(j,1)),',',int2str(P_A(j,2))));
+            end
+            % replace this with sending coordinates later
+            
+            % update last length
+        end 
+        % clear the coordinate array    
+    else
+        imshow(img);
     end
-    % display image and any centroids if there are any
-    imshow(img);
-    
-    
-    
-    if length(xLeftTop) > 0 
-        hold on;
-        plot(xLeftTop , yLeftBottom  , 'r*', 'LineWidth', 1, 'MarkerSize', 2);
-    end 
-    %hold on;
-    
-    
+    % replace display to output coordinates instead
+    % when you need to modularize this code
+    disp(coordinates);
+    disp("\n");
+    coordinates = {};
 end 
 %% 
 clear cam;
 
-%% Supporting Functions
-% arguments: 
-% the vector from camera to the robot reference frame origin,
-% the vector from camera frame to the target
-% angles: theta, psi, phi, in angles
-% outputs:
-% 3x1 vector describing targets position in robot reference frame, P_r_t
+function P_A = mapToRobotBase(theta,phi,psi,xCenter,yCenter,P_A_BORG)
 
-function P_A = mapForRobotPose(theta,phi,psi,P_B,P_A_BORG)
-
-pixelToInches = 0.1497; % constant subject to change, dependant on the how
-                        % you place the camera and its measurements
-theta = 0;
-phi = 0;
-psi = 0;
-
-xInInches = xLeftTop * pixelToInches
-yInInches = yLeftBottom * pixelToInches
+xPixelToInches = 0.1919; % constant subject to change, dependant on the how
+yPixelToInches = 0.1094;
+% you place the camera and its measurements
+xInInches = xCenter * xPixelToInches;
+yInInches = yCenter * yPixelToInches;
 
 P_B = [ xInInches; yInInches; 0; 1]; % vector from camera to target
-P_A_BORG = [ 30; -30; 0 ]; % vector from robot to camera
 
 Rx = [ 1 0 0;
      0 cosd(theta) -sind(theta) ;
@@ -80,7 +79,8 @@ Ry = [ cosd(phi) 0 sind(phi);
 Rz = [ cosd(psi) -sind(psi) 0 ;
      sind(psi) cosd(psi) 0;
      0 0 1];
-% roatation from camera to robot
+ 
+% rotation from camera to robot
 R_A_B =  Rz * Ry * Rx;
 T_A_B(1:3,1:3) = R_A_B;
 T_A_B(1:3,4) = P_A_BORG;
